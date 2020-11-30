3168fe446fe064023b6aef0b3b0faa933fabb6f2
Aleksandrs Smirnovs <smirnoffchess@gmail.com>
2020-11-30 20:20:45 +0200

    c

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, JAXBException {
         AbstractXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF\\spring\\camel-context.xml");
         context.start();
-        Thread.sleep(5000);
+        Thread.sleep(3000);
         context.stop();
 
 
